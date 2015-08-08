0f18f5609f1f0954bc82d79401946d4f0da729e1
Eric Eilebrecht <ericeil@microsoft.com>
2015-08-07 17:08:02 -0700

    Speed up statistical stuff

--- xunit.performance/BenchmarkTestInvoker.cs
+++ xunit.performance/BenchmarkTestInvoker.cs
@@ -1,4 +1,5 @@
-﻿using MathNet.Numerics.Statistics;
+﻿using MathNet.Numerics;
+using MathNet.Numerics.Statistics;
 using Microsoft.Xunit.Performance.Analysis;
 using System;
 using System.Collections.Generic;
@@ -320,11 +321,25 @@ namespace Microsoft.Xunit.Performance
                 return false;
 
             var stderr = stats.StandardDeviation / Math.Sqrt(stats.Count);
-            var t = MathNet.Numerics.ExcelFunctions.TInv(1.0 - confidence, (int)stats.Count - 1);
+            var t = TInv(1.0 - confidence, (int)stats.Count - 1);
             var mean = stats.Mean;
             var interval = t * stderr;
 
             return (interval / mean) < marginOfError;
+        }
+
+        [ThreadStatic]
+        private static Dictionary<double, Dictionary<int, double>> _TInvCache = new Dictionary<double, Dictionary<int, double>>();
+
+        private static double TInv(double probability, int degreesOfFreedom)
+        {
+            Dictionary<int, double> dofCache;
+            if (!_TInvCache.TryGetValue(probability, out dofCache))
+                _TInvCache[probability] = dofCache = new Dictionary<int, double>();
+            double result;
+            if (!dofCache.TryGetValue(degreesOfFreedom, out result))
+                dofCache[degreesOfFreedom] = result = ExcelFunctions.TInv(probability, degreesOfFreedom);
+            return result;
         }
     }
 }
