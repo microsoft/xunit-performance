83da7e187566a26295f285791b1170585a75b7ea
Paul Harrington <pharring@microsoft.com>
2015-08-14 12:34:19 -0700

    Renamed xunit.performance to xunit.performance.core and made it portable.

--- xunit.performance.core/Properties/AssemblyInfo.cs
+++ xunit.performance.core/Properties/AssemblyInfo.cs
@@ -1,26 +1,20 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("xunit.performance")]
+[assembly: AssemblyTitle("xunit.performance.core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("xunit.performance")]
+[assembly: AssemblyProduct("xunit.performance.core")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("67071a0f-fe22-4eea-86e5-419e9eb4c1bd")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
