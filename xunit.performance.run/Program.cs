afd1538f82310a3e7b37c82a4d1d5bbdba06920b
Eric Eilebrecht <ericeil@hotmail.com>
2015-08-13 23:07:04 -0700

    Move DictionaryExtensions to its own file.

--- xunit.performance.run/Program.cs
+++ xunit.performance.run/Program.cs
@@ -309,49 +309,4 @@ namespace Microsoft.Xunit.Performance
                 throw new ArgumentException($"error: unknown command line option: {option.Value}");
         }
     }
-
-    internal static class DictionaryExtensions
-    {
-        public static void Add<TKey, TValue>(this IDictionary<TKey, List<TValue>> dictionary, TKey key, TValue value)
-        {
-            dictionary.GetOrAdd(key).Add(value);
-        }
-
-        public static bool Contains<TKey, TValue>(this IDictionary<TKey, List<TValue>> dictionary, TKey key, TValue value, IEqualityComparer<TValue> valueComparer)
-        {
-            List<TValue> values;
-
-            if (!dictionary.TryGetValue(key, out values))
-                return false;
-
-            return values.Contains(value, valueComparer);
-        }
-
-        public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
-            where TValue : new()
-        {
-            return dictionary.GetOrAdd<TKey, TValue>(key, () => new TValue());
-        }
-
-        public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> newValue)
-        {
-            TValue result;
-
-            if (!dictionary.TryGetValue(key, out result))
-            {
-                result = newValue();
-                dictionary[key] = result;
-            }
-
-            return result;
-        }
-
-        public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue = default(TValue))
-        {
-            TValue value;
-            if (!dictionary.TryGetValue(key, out value))
-                return defaultValue;
-            return value;
-        }
-    }
 }
