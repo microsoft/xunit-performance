d0e953da4483799d64aa7724f6438c3e97acbf7f
Paul Harrington <pharring@microsoft.com>
2015-08-15 22:21:45 -0700

    Add MakeNugetPackages script

---
+++ MakeNugetPackages.cmd
@@ -0,0 +1,3 @@
+.nuget\nuget.exe pack xunit.performance.nuspec -NoPackageAnalysis -NonInteractive -Verbosity quiet
+.nuget\nuget.exe pack xunit.performance.core.nuspec -NoPackageAnalysis -NonInteractive -Verbosity quiet
+.nuget\nuget.exe pack xunit.performance.execution.nuspec -NoPackageAnalysis -NonInteractive -Verbosity quiet
