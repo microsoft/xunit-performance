c18cadf4d0b73d7bebe97c841199264c03d1f0ba
Eric Eilebrecht <ericeil@microsoft.com>
2015-08-18 11:46:20 -0700

    Add IPerformanceMetricDiscoverer.GetMetrics See #10.

--- src/xunit.performance.core/IPerformanceMetricDiscoverer.cs
+++ src/xunit.performance.core/IPerformanceMetricDiscoverer.cs
@@ -1,9 +1,17 @@
-﻿namespace Microsoft.Xunit.Performance
+﻿using System.Collections.Generic;
+
+namespace Microsoft.Xunit.Performance
 {
     /// <summary>
     /// Implemented by discoverers that provide metrics to performance tests.
     /// </summary> 
     public interface IPerformanceMetricDiscoverer
     {
+        /// <summary>
+        /// Gets the performance metrics from the metric attribute.
+        /// </summary>
+        /// <param name="metricAttribute"></param>
+        /// <returns></returns>
+        IEnumerable<IPerformanceMetric> GetMetrics(IPerformanceMetricAttribute metricAttribute);
     }
 }
