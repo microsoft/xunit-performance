b6ee96adf1571f8cc9c034a1574158ec7ad653f7
Eric Eilebrecht <ericeil@microsoft.com>
2015-08-18 11:43:21 -0700

    Add IPerformanceMetric. Part of addressing #10.

---
+++ src/xunit.performance.core/IPerformanceMetric.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.Xunit.Performance
+{
+    /// <summary>
+    /// Implemented by types which provide metrics for performance tests.
+    /// </summary>
+    public interface IPerformanceMetric
+    {
+        string DisplayName { get; }
+    }
+}
