14a7909d56e6c4ba76df15c223e81531da0c99c7
Eric Eilebrecht <ericeil@microsoft.com>
2015-08-20 11:27:37 -0700

    Sort usings

--- src/xunit.performance.run/PerformanceTestDisoveryVisitor.cs
+++ src/xunit.performance.run/PerformanceTestDisoveryVisitor.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.Xunit.Performance.Sdk;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Microsoft.Xunit.Performance.Sdk;
 using Xunit;
 using Xunit.Abstractions;
 using Xunit.Sdk;
