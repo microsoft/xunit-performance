e61aeabc65020d0c87bdcbdb1a882255510ece48
Eric Eilebrecht <ericeil@microsoft.com>
2015-08-20 10:45:36 -0700

    Consolodate duration metric into BenchmarkMetricDiscoverer

--- src/xunit.performance.metrics/BenchmarkMetricDiscoverer.cs
+++ src/xunit.performance.metrics/BenchmarkMetricDiscoverer.cs
@@ -2,13 +2,46 @@
 using System.Collections.Generic;
 using Microsoft.Xunit.Performance.Sdk;
 using Xunit.Abstractions;
+using Microsoft.Diagnostics.Tracing;
 
 namespace Microsoft.Xunit.Performance
 {
     class BenchmarkMetricDiscoverer : IPerformanceMetricDiscoverer
     {
-        static readonly PerformanceMetric[] _metrics = new[] { new BenchmarkDurationMetric() };
+        public IEnumerable<PerformanceMetric> GetMetrics(IAttributeInfo metricAttribute)
+        {
+            yield return new BenchmarkDurationMetric();
+        }
 
-        public IEnumerable<PerformanceMetric> GetMetrics(IAttributeInfo metricAttribute) => _metrics;
+        class BenchmarkDurationMetric : PerformanceMetric
+        {
+            public BenchmarkDurationMetric() 
+                : base("Duration", "msec", PerformanceMetricInterpretation.LowerIsBetter)
+            {
+            }
+
+            public override PerformanceMetricEvaluator CreateEvaluator(PerformanceMetricEvaluationContext context)
+            {
+                return new BenchmarkDurationEvaluator(context);
+            }
+        }
+        internal class BenchmarkDurationEvaluator : PerformanceMetricEvaluator
+        {
+            double _iterationStartRelativeMSec;
+
+            public BenchmarkDurationEvaluator(PerformanceMetricEvaluationContext context)
+            {
+            }
+
+            public override void BeginIteration(TraceEvent beginEvent)
+            {
+                _iterationStartRelativeMSec = beginEvent.TimeStampRelativeMSec;
+            }
+
+            public override double EndIteration(TraceEvent endEvent)
+            {
+                return endEvent.TimeStampRelativeMSec - _iterationStartRelativeMSec;
+            }
+        }
     }
 }
