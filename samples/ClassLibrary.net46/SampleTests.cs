6a524e5b9a4e08254669f0a3cf844d1a125a0824
Paul Harrington <pharring@microsoft.com>
2015-08-20 11:54:50 -0700

    Introduce samples solution referencing Nuget packages

---
+++ samples/ClassLibrary.net46/SampleTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xunit.Performance;
+using Xunit;
+
+namespace ClassLibrary.net46
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void AlwaysPass()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void AlwaysFail()
+        {
+            Assert.True(false);
+        }
+
+        [Benchmark]
+        public void Benchmark1()
+        {
+        }
+
+        [Benchmark]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void Benchmark2(int x)
+        {
+        }
+    }
+}
