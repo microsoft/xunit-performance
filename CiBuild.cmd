023ba75060d55539246f75e24938fd33da6cd1e5
Paul Harrington <pharring@microsoft.com>
2015-08-28 12:14:58 -0700

    Add CiBuild.cmd

---
+++ CiBuild.cmd
@@ -0,0 +1,22 @@
+@echo off
+setlocal
+
+set BuildConfiguration=Debug
+
+:ParseArguments
+if "%1" == "" goto :DoneParsing
+if /I "%1" == "/?" call :Usage && exit /b 1
+if /I "%1" == "/debug" set BuildConfiguration=Debug&&shift&& goto :ParseArguments
+if /I "%1" == "/release" set BuildConfiguration=Release&&shift&& goto :ParseArguments
+call :Usage && exit /b 1
+:DoneParsing
+
+msbuild.exe /nologo /v:m /m /p:Configuration=%BuildConfiguration% /t:CI /fl xunit.performance.msbuild
+
+goto :eof
+
+:Usage
+@echo Usage: CiBuild.cmd [/debug^|/release]
+@echo   /debug 	Perform debug build.  This is the default.
+@echo   /release Perform release build
+@goto :eof
--- src/common/GlobalAssemblyInfo.cs
+++ src/common/GlobalAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Reflection;
 [assembly: AssemblyCompany("Microsoft Corporation")]
 [assembly: AssemblyProduct("xUnit Performance Testing Framework")]
 [assembly: AssemblyCopyright("Copyright \u00A9 Microsoft Corporation 2015")]
-[assembly: AssemblyVersion("1.0.0.10")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha-build0010")]
+[assembly: AssemblyVersion("99.99.99.0")]
+[assembly: AssemblyInformationalVersion("99.99.99-dev")]
