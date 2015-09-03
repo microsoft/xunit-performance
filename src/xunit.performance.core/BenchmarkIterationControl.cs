2eba4151c7d05077ebb0901a6a3d96d1cc5aaf9e
Eric Eilebrecht <ericeil@microsoft.com>
2015-09-02 17:12:51 -0700

    Add rough sketch of manual iteration control.

---
+++ src/xunit.performance.core/BenchmarkIterationControl.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Xunit.Performance
+{
+    /// <summary>
+    /// Allows a performance test to manually control test iterations.
+    /// </summary>
+    public class BenchmarkIterationControl
+    {
+        /// <summary>
+        /// Returns true if the test should execute at least one more iteration.
+        /// </summary>
+        public bool NeedMoreIterations { get; }
+
+        /// <summary>
+        /// Provides a <see cref="CancellationToken"/> that will enter the cancelled state when no more iterations are needed.
+        /// </summary>
+        public CancellationToken CancellationToken { get; }
+
+        /// <summary>
+        /// Called by the test method to indicate that an iteration is starting.
+        /// </summary>
+        /// <remarks>
+        /// When the iteration is complete, call <see cref="RunningBenchmarkIteration.Dispose"/>.
+        /// </remarks>
+        /// <returns>A <see cref="RunningBenchmarkIteration"/> representing this iteration.</returns>
+        /// <param name="iteration">The ordinal of this iteration.  If zero, this is a "warmup" iteration that should be ignored by analysis.</param>
+        public RunningBenchmarkIteration StartIteration(int iteration)
+        {
+            return new RunningBenchmarkIteration();
+        }
+    }
+
+    /// <summary>
+    /// Represents a benchmark iteration that is currently executing.
+    /// </summary>
+    /// <remarks>
+    /// Call <see cref="Dispose"/> when the iteration is complete.
+    /// </remarks>
+    public struct RunningBenchmarkIteration : IDisposable
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ src/xunit.performance.core/BenchmarkType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Xunit.Performance
+{
+    /// <summary>
+    /// Controls the execution of a method marked with <see cref="BenchmarkAttribute"/>.
+    /// </summary>
+    public enum BenchmarkType
+    {
+        /// <summary>
+        /// Indicates that the method is a simple benchmark, with iterations controlled by the xunit.performance framework.
+        /// </summary>
+        Simple,
+
+        /// <summary>
+        /// Indicates that the method requires manual control over iterations.
+        /// </summary>
+        /// <remarks>
+        /// The method's first argument must be of type BenchmarkIterationControl.
+        /// </remarks>
+        ManualControl
+    }
+}
