ac55bf8c534ff1b27f3613cababea47225eea20c
Eric Eilebrecht <ericeil@microsoft.com>
2015-09-28 13:57:19 -0700

    Fix warnings about missing XML comments.

--- src/xunit.performance.core/PerformanceMetricUnits.cs
+++ src/xunit.performance.core/PerformanceMetricUnits.cs
@@ -9,10 +9,24 @@ using System.Threading.Tasks;
 
 namespace Microsoft.Xunit.Performance.Sdk
 {
+    /// <summary>
+    /// Provides standard strings for <see cref="PerformanceMetricInfo.Unit"/>
+    /// </summary>
     public static class PerformanceMetricUnits
     {
+        /// <summary>
+        /// Indicates that a performance metric's value is a unitless count.
+        /// </summary>
         public const string Count = "count";
+
+        /// <summary>
+        /// Indicates that a performance metric's value is measures in milliseconds.
+        /// </summary>
         public const string Milliseconds = "msec";
+
+        /// <summary>
+        /// Indicates that a performance metric's value is measured in megabytes (1,048,576 bytes).
+        /// </summary>
         public const string Megabytes = "mbyte";
     }
 }
