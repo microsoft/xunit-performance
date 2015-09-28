8651701a7901b028c08608fb50b57110ec448c95
Eric Eilebrecht <ericeil@microsoft.com>
2015-09-28 11:36:13 -0700

    Fix BuildForPublishcation.cmd Apparently the paths in nuget.config are relative to the *current* directory...

--- BuildForPublication.cmd
+++ BuildForPublication.cmd
@@ -2,5 +2,7 @@
 setlocal
 call NightlyBuild.cmd
 set PackageDir=%~dp0LocalPackages
-call dnu restore dnx\src\Microsoft.DotNet.xunit.performance.runner.dnx\project.json -f %PackageDir%
-call dnu pack dnx\src\Microsoft.DotNet.xunit.performance.runner.dnx\project.json --out %PackageDir% --configuration Release
+pushd %~dp0dnx\src\Microsoft.DotNet.xunit.performance.runner.dnx
+call dnu restore
+call dnu pack --out %PackageDir% --configuration Release
+popd
