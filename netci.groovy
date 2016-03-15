38b4b5052f751ababa35d238e0ebe1a47af8eb5e
Matt Mitchell <mmitche@microsoft.com>
2016-03-15 14:33:30 -0700

    Update netci to use more modern style
    Update to simplify and modernize the netci

--- netci.groovy
+++ netci.groovy
@@ -2,60 +2,28 @@
 
 import jobs.generation.Utilities;
 
-// **************************
-// Define code coverage build
-// **************************
+def project = GithubProject
+def branch = GithubBranchName
 
-def project = 'Microsoft/xunit-performance'
-// Define build string
-def debugBuildString = '''call "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\Tools\\VsDevCmd.bat" && CIBuild /debug'''
-def releaseBuildString = '''call "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\Tools\\VsDevCmd.bat" && CIBuild /release'''
+// Standard build jobs
 
-// Generate the builds for debug and release
-
-def windowsDebugJob = job(Utilities.getFullJobName(project, 'debug', false)) {
-  label('windows')
-  steps {
-    batchFile(debugBuildString)
-  }
-}
-
-def windowsReleaseJob = job(Utilities.getFullJobName(project, 'release', false)) {
-  label('windows')
-  steps {
-    batchFile(releaseBuildString)
-  }
-}
-             
-def windowsDebugPRJob = job(Utilities.getFullJobName(project, 'debug', true)) {
-  label('windows')
-  steps {
-    batchFile(debugBuildString)
-  }
-}
-
-Utilities.addGithubPRTrigger(windowsDebugPRJob, 'Debug Build')
-
-def windowsReleasePRJob = job(Utilities.getFullJobName(project, 'release', true)) {
-  label('windows')
-  steps {
-    batchFile(releaseBuildString)
-  }
-}
-
-Utilities.addGithubPRTrigger(windowsReleasePRJob, 'Release Build')
-
-[windowsDebugJob, windowsReleaseJob].each { commitJob ->
-    Utilities.addScm(commitJob, project)
-    Utilities.addStandardOptions(commitJob)
-    Utilities.addStandardNonPRParameters(commitJob)
-    Utilities.addGithubPushTrigger(commitJob)
-    Utilities.addArchival(commitJob, 'msbuild.log', '', true, false)
-}
-
-[windowsDebugPRJob, windowsReleasePRJob].each { PRJob ->
-    Utilities.addPRTestSCM(PRJob, project)
-    Utilities.addStandardOptions(PRJob)
-    Utilities.addStandardPRParameters(PRJob, project)
-    Utilities.addArchival(PRJob, 'msbuild.log', '', true, false)
+[true, false].each { isPR ->
+    ['Debug','Release'].each { configuration ->
+        def lowerConfigurationName = configuration.toLowerCase();
+        def newJob = job(Utilities.getFullJobName(project, lowerConfigurationName, isPR)) {
+            steps {
+                batchFile("call \"C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\Tools\\VsDevCmd.bat\" && CIBuild /${lowerConfigurationName}")
+            }
+        }
+        
+        Utilities.setMachineAffinity(newJob, 'Windows_NT', 'latest-or-auto')
+        Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
+        Utilities.addArchival(newJob, 'msbuild.log', '', true, false)
+        if (isPR) {
+            Utilities.addGithubPRTriggerForBranch(newJob, branch, "${configuration} Build")
+        }
+        else {
+            Utilities.addGithubPushTrigger(newJob)
+        }
+    }
 }
