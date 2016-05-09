7f448ef48be33e785aa5ecb8118fe9826480af46
Brian Robbins <brianrob@microsoft.com>
2016-05-09 12:17:14 -0700

    Add support for building against Ubuntu in the CI.

--- netci.groovy
+++ netci.groovy
@@ -8,22 +8,31 @@ def branch = GithubBranchName
 // Standard build jobs
 
 [true, false].each { isPR ->
-    ['Debug','Release'].each { configuration ->
-        def lowerConfigurationName = configuration.toLowerCase();
-        def newJob = job(Utilities.getFullJobName(project, lowerConfigurationName, isPR)) {
-            steps {
-                batchFile("call \"C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\Tools\\VsDevCmd.bat\" && CIBuild /${lowerConfigurationName}")
+    ['Windows_NT', 'Ubuntu'] { os ->
+        ['Debug','Release'].each { configuration ->
+            def lowerConfigurationName = configuration.toLowerCase();
+            def newJob = job(Utilities.getFullJobName(project, lowerConfigurationName, isPR)) {
+                steps {
+                    if (os == 'Windows_NT') {
+                        batchFile("call \"C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\Tools\\VsDevCmd.bat\" && CIBuild /${lowerConfigurationName}")
+                    }
+                    else if (os == 'Ubuntu') {
+                        shell("uname -a")
+                    }
+                }
             }
-        }
         
-        Utilities.setMachineAffinity(newJob, 'Windows_NT', 'latest-or-auto')
-        Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
-        Utilities.addArchival(newJob, 'msbuild.log', '', true, false)
-        if (isPR) {
-            Utilities.addGithubPRTriggerForBranch(newJob, branch, "${configuration} Build")
-        }
-        else {
-            Utilities.addGithubPushTrigger(newJob)
+            Utilities.setMachineAffinity(newJob, ${os}, 'latest-or-auto')
+            Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
+            if (os == 'Windows_NT') {
+                Utilities.addArchival(newJob, 'msbuild.log', '', true, false)
+            }
+            if (isPR) {
+                Utilities.addGithubPRTriggerForBranch(newJob, branch, "${os} ${configuration} Build")
+            }
+            else {
+                Utilities.addGithubPushTrigger(newJob)
+            }
         }
     }
 }
