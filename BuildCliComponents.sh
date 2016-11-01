0ca89b3ad1c8014274f70d50490deab729fd2541
Brian Robbins <brianrob@microsoft.com>
2016-11-01 16:54:48 -0700

    Migrate all projects to build with the CLI and upgrade to CLI 1.0.0-preview3-004056.

--- BuildCliComponents.sh
+++ BuildCliComponents.sh
@@ -17,7 +17,7 @@ declare currentDir=`pwd`
 declare outputDirectory=${currentDir}/LocalPackages
 declare dotnetPath=${currentDir}/tools/bin/ubuntu
 declare dotnetCmd=${dotnetPath}/dotnet
-declare dotnetInstallerUrl=https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0-preview2/scripts/obtain/dotnet-install.sh
+declare dotnetInstallerUrl=https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh
 declare dotnetInstallerScript=${dotnetPath}/dotnet-install.sh
 declare dotnetVersion=`cat DotNetCliVersion.txt`
 
@@ -47,23 +47,20 @@ then
 	buildConfiguration="debug"
 fi
 
-declare nupkgCount=`ls LocalPackages/*.nupkg | wc -l`
-if [ "$nupkgCount" == "0"  ]
+declare versionSuffix=$2
+if [ "$versionSuffix" == "" ]
 then
-	echo Linux builds depend on artifacts from Windows build.  Please build on Windows first.
-	exit -1
+	versionSuffix="build0000"
 fi
 
-echo Building CLI-based components
-pushd ${currentDir}/src/cli/Microsoft.DotNet.xunit.performance.runner.cli > /dev/null
 $dotnetCmd restore
-$dotnetCmd build -c $buildConfiguration
-$dotnetCmd pack -c $buildConfiguration -o $outputDirectory
+pushd ${currentDir}/src/cli/Microsoft.DotNet.xunit.performance.runner.cli > /dev/null
+$dotnetCmd build -c $buildConfiguration --version-suffix $versionSuffix
+$dotnetCmd pack -c $buildConfiguration -o $outputDirectory --version-suffix $versionSuffix
 popd > /dev/null
 pushd ${currentDir}/src/cli/Microsoft.DotNet.xunit.performance.analysis.cli > /dev/null
-$dotnetCmd restore
-$dotnetCmd build -c $buildConfiguration
-$dotnetCmd pack -c $buildConfiguration -o $outputDirectory
+$dotnetCmd build -c $buildConfiguration --version-suffix $versionSuffix
+$dotnetCmd pack -c $buildConfiguration -o $outputDirectory --version-suffix $versionSuffix
 popd > /dev/null
 
 echo Build complete
