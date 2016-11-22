2f885816641225f34de77e812f3f8551821da495
Jose Rivero <jorive@microsoft.com>
2016-11-22 09:22:50 -0800

    Output a csv file with performance statistics.

--- src/xunit.performance.api/XunitPerformanceHarness.cs
+++ src/xunit.performance.api/XunitPerformanceHarness.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Microsoft.Xunit.Performance;
+using Microsoft.Xunit.Performance.Api.Table;
 
 namespace Microsoft.Xunit.Performance.Api
 {
@@ -38,7 +40,7 @@ namespace Microsoft.Xunit.Performance.Api
 
         public void RunBenchmarks(string assemblyPath)
         {
-            if(string.IsNullOrEmpty(assemblyPath))
+            if (string.IsNullOrEmpty(assemblyPath))
             {
                 throw new ArgumentNullException(nameof(assemblyPath));
             }
@@ -52,19 +54,90 @@ namespace Microsoft.Xunit.Performance.Api
         {
             CSVMetricReader reader = new CSVMetricReader(Configuration.FileLogPath);
 
-            foreach(string testCaseName in reader.TestCases)
+            foreach (string testCaseName in reader.TestCases)
             {
                 Console.WriteLine($"\nTest Case: {testCaseName}");
                 List<Dictionary<string, double>> iterations = reader.GetValues(testCaseName);
-                for(int i=0; i<iterations.Count; i++)
+
+                for (int i = 0; i < iterations.Count; i++)
                 {
                     Dictionary<string, double> iter = iterations[i];
-                    foreach(KeyValuePair<string, double> result in iter)
+                    foreach (KeyValuePair<string, double> result in iter)
                     {
                         Console.WriteLine($"Iter={i}; Metric={result.Key}; Value={result.Value}");
                     }
                 }
             }
+
+            WriteStatisticsToFile(reader);
+        }
+
+        /// <summary>
+        /// This method writes the performance results to csv files.
+        /// </summary>
+        /// <param name="reader"></param>
+        private void WriteStatisticsToFile(CSVMetricReader reader)
+        {
+            //Console.WriteLine();
+            //Console.Write("[DEBUG] Press <Enter> to continue...");
+            //Console.ReadLine();
+
+            /*
+             * Generate CSV data (Probably not the most efficient way, e.g. How big can this become).
+             *  Test Name,Metric,Iterations,Average,Sample standard deviation,Minimum,Maximum
+             */
+            //var rawFilePath = $"{Configuration.RunId}-raw.csv";
+            //var rawTable = new DataTable();
+
+            var summaryFilePath = $"{Configuration.RunId}-Summary.csv";
+            var summaryTable = new DataTable();
+            var col0 = summaryTable.AddColumn("Test Name");
+            var col1 = summaryTable.AddColumn("Metric");
+            var col2 = summaryTable.AddColumn("Iterations");
+            var col3 = summaryTable.AddColumn("Average");
+            var col4 = summaryTable.AddColumn("Sample standard deviation");
+            var col5 = summaryTable.AddColumn("Minimum");
+            var col6 = summaryTable.AddColumn("Maximum");
+
+            foreach (string testCaseName in reader.TestCases)
+            {
+                List<Dictionary<string, double>> iterations = reader.GetValues(testCaseName);
+
+                var measurements = new Dictionary<string, List<double>>();
+                foreach (var dict in iterations)
+                {
+                    foreach (var pair in dict)
+                    {
+                        if (!measurements.ContainsKey(pair.Key))
+                            measurements[pair.Key] = new List<double>();
+                        measurements[pair.Key].Add(pair.Value);
+                    }
+                }
+
+                foreach (var measurement in measurements)
+                {
+                    var metric = measurement.Key;
+                    var count = measurement.Value.Count;
+                    var avg = measurement.Value.Average();
+                    var stdev_s = Math.Sqrt(measurement.Value.Sum(x => Math.Pow(x - avg, 2)) / (measurement.Value.Count - 1));
+                    var max = measurement.Value.Max();
+                    var min = measurement.Value.Min();
+
+                    var r = summaryTable.AppendRow();
+                    r[col0] = testCaseName;
+                    r[col1] = metric;
+                    r[col2] = count.ToString();
+                    r[col3] = avg.ToString();
+                    r[col4] = stdev_s.ToString();
+                    r[col5] = min.ToString();
+                    r[col6] = max.ToString();
+
+                    //Console.WriteLine($"{testCaseName},{metric},{count},{avg},{stdev_s},{min},{max}");
+                }
+            }
+
+            summaryTable.WriteToCSV(summaryFilePath);
+            Console.WriteLine($"Created \"{summaryFilePath}\"");
         }
     }
 }
