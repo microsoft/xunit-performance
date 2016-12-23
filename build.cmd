6efe624e42049cd5e79c3de96a4cace48fd50070
Jose Rivero <jorive@microsoft.com>
2016-12-22 19:49:25 -0800

    [Backup] 1. Added error hadling to the build scripts 2. Some code cleanup 3. Removed the dependency to TraceEventCore.csproj (different repo) from our projects     Temporarily added a local feed to a local nuget package 4. Added markdown output to the console from the performance statistics 5. Cleanup some csproj files

--- build.cmd
+++ build.cmd
@@ -1,108 +1,204 @@
 @echo off
-goto :main
+@if defined _echo echo on
 
 :main
 setlocal
+  set errorlevel=
 
-set BuildConfiguration=%1%
-if "%BuildConfiguration%"=="" set BuildConfiguration=Debug
+  set BuildConfiguration=%~1
+  if "%BuildConfiguration%"=="" set BuildConfiguration=Debug
 
-set VersionSuffix=%2%
-if "%VersionSuffix%"=="" set VersionSuffix=build0000
+  set VersionSuffix=%~2
+  if "%VersionSuffix%"=="" set VersionSuffix=build0000
 
-set PackageVersion=%3%
-if "%PackageVersion%"=="" set PackageVersion=1.0.0-alpha-build0000
+  set PackageVersion=%~3
+  if "%PackageVersion%"=="" set PackageVersion=1.0.0-alpha-build0000
 
-set OutputDirectory=%~dp0LocalPackages
-set DotNet=%~dp0\tools\bin\dotnet.exe
+  set OutputDirectory=%~dp0LocalPackages
+  call :remove_directory "%OutputDirectory%" || exit /b 1
 
-if exist "%DotNet%" goto :build
-echo Installing Dotnet CLI
+  set DotNet=%~dp0\tools\bin\dotnet.exe
 
-set DotNet_Path=%~dp0tools\bin
-set Init_Tools_Log=%DotNet_Path%\install.log
+  if not exist "%DotNet%" (
+    call :install_dotnet_cli || exit /b 1
+  )
 
-if NOT exist "%DotNet_Path%" mkdir "%DotNet_Path%"
+  set procedures=
+  set procedures=%procedures% build_procdomain
+  set procedures=%procedures% build_xunit_performance_analysis
+  set procedures=%procedures% build_xunit_performance_core
+  set procedures=%procedures% build_xunit_performance_execution
+  set procedures=%procedures% build_xunit_performance_logger
+  set procedures=%procedures% build_xunit_performance_metrics
+  set procedures=%procedures% build_xunit_performance_run
+  set procedures=%procedures% build_microsoft_dotnet_xunit_performance_runner_cli
+  set procedures=%procedures% build_microsoft_dotnet_xunit_performance_analysis_cli
+  set procedures=%procedures% build_samples_classlibrary_net46
+  set procedures=%procedures% build_samples_simpleperftests
+  set procedures=%procedures% nuget_pack_src
+  set procedures=%procedures% build_xunit_performance_api
+  set procedures=%procedures% build_tests_simpleharness
 
-set /p DotNet_Version=< %~dp0DotNetCLIVersion.txt
-set DotNet_Installer_Url=https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1
+  for %%p in (%procedures%) do (
+    call :%%p || (
+      call :print_error_message Failed to run %%p
+      exit /b 1
+    )
+  )
+  exit /b %errorlevel%
 
-powershell -NoProfile -ExecutionPolicy unrestricted -Command "Invoke-WebRequest -Uri '%DotNet_Installer_Url%' -OutFile '%DotNet_Path%\dotnet-install.ps1'"
-echo Executing dotnet installer script %DotNet_Path%\dotnet-install.ps1
-powershell -NoProfile -ExecutionPolicy unrestricted -Command "%DotNet_Path%\dotnet-install.ps1 -InstallDir %DotNet_Path% -Version '%DotNet_Version%'"
+:install_dotnet_cli
+setlocal
+  echo Installing Dotnet CLI
 
-if NOT exist "%DotNet%" (
-  echo ERROR: Could not install dotnet cli correctly. See '%Init_Tools_Log%' for more details.
-  goto :EOF
-)
+  set DotNet_Path=%~dp0tools\bin
+  set Init_Tools_Log=%DotNet_Path%\install.log
 
-:build
-pushd %~dp0src\procdomain
-call %DotNet% restore
-call %DotNet% build -c %BuildConfiguration% --version-suffix %VersionSuffix%
-popd
-pushd %~dp0src\xunit.performance.analysis
-call %DotNet% restore
-call %DotNet% build -c %BuildConfiguration% --version-suffix %VersionSuffix%
-call %DotNet% pack -c %BuildConfiguration% --version-suffix %VersionSuffix% --output %OutputDirectory% --include-symbols --include-source
-popd
-pushd %~dp0src\xunit.performance.core
-call %DotNet% restore
-call %DotNet% build -c %BuildConfiguration% --version-suffix %VersionSuffix%
-call %DotNet% pack -c %BuildConfiguration% --version-suffix %VersionSuffix% --output %OutputDirectory% --include-symbols --include-source
-popd
-pushd %~dp0src\xunit.performance.execution
-call %DotNet% restore
-call %DotNet% build -c %BuildConfiguration% --version-suffix %VersionSuffix%
-call %DotNet% pack -c %BuildConfiguration% --version-suffix %VersionSuffix% --output %OutputDirectory% --include-symbols --include-source
-popd
-pushd %~dp0src\xunit.performance.logger
-call %DotNet% restore
-call %DotNet% build -c %BuildConfiguration% --version-suffix %VersionSuffix%
-popd
-pushd %~dp0src\xunit.performance.metrics
-call %DotNet% restore
-call %DotNet% build -c %BuildConfiguration% --version-suffix %VersionSuffix%
-call %DotNet% pack -c %BuildConfiguration% --version-suffix %VersionSuffix% --output %OutputDirectory% --include-symbols --include-source
-popd
-pushd %~dp0src\xunit.performance.run
-call %DotNet% restore
-call %DotNet% build -c %BuildConfiguration% --version-suffix %VersionSuffix%
-popd
-pushd %~dp0src\cli\Microsoft.DotNet.xunit.performance.runner.cli
-call %DotNet% restore
-call %DotNet% build -c %BuildConfiguration% --version-suffix %VersionSuffix%
-call %DotNet% pack -c %BuildConfiguration% --version-suffix %VersionSuffix% --output %OutputDirectory% --include-symbols --include-source
-popd
-pushd %~dp0src\cli\Microsoft.DotNet.xunit.performance.analysis.cli
-call %DotNet% restore
-call %DotNet% build -c %BuildConfiguration% --version-suffix %VersionSuffix%
-call %DotNet% pack -c %BuildConfiguration% --version-suffix %VersionSuffix% --output %OutputDirectory% --include-symbols --include-source
-popd
-pushd %~dp0samples\ClassLibrary.net46
-call %DotNet% restore
-call %DotNet% build -c %BuildConfiguration% --version-suffix %VersionSuffix%
-popd
-pushd %~dp0samples\SimplePerfTests
-call %DotNet% restore
-call %DotNet% build -c %BuildConfiguration% --version-suffix %VersionSuffix%
-popd
+  if not exist "%DotNet_Path%" mkdir "%DotNet_Path%"
 
-pushd %~dp0src
-call %DotNet% nuget pack xunit.performance.nuspec -p Configuration=%BuildConfiguration% --version=%PackageVersion% --output-directory %OutputDirectory% --symbols
-call %DotNet% nuget pack xunit.performance.runner.Windows.nuspec -p Configuration=%BuildConfiguration% --version=%PackageVersion% --output-directory %OutputDirectory% --symbols
-popd
+  set /p DotNet_Version=< %~dp0DotNetCLIVersion.txt
+  set DotNet_Installer_Url=https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1
 
-pushd %~dp0src\xunit.performance.api
-call %DotNet% restore
-call %DotNet% build -c %BuildConfiguration% --version-suffix %VersionSuffix%
-call %DotNet% pack -c %BuildConfiguration% --version-suffix %VersionSuffix% --output %OutputDirectory% --include-symbols --include-source
-popd
+  powershell -NoProfile -ExecutionPolicy unrestricted -Command "Invoke-WebRequest -Uri '%DotNet_Installer_Url%' -OutFile '%DotNet_Path%\dotnet-install.ps1'"
+  echo Executing dotnet installer script %DotNet_Path%\dotnet-install.ps1
+  powershell -NoProfile -ExecutionPolicy unrestricted -Command "%DotNet_Path%\dotnet-install.ps1 -InstallDir %DotNet_Path% -Version '%DotNet_Version%'"
 
-pushd %~dp0tests\simpleharness
-call %DotNet% restore
-call %DotNet% build -c %BuildConfiguration% --version-suffix %VersionSuffix%
-call %DotNet% bin\%BuildConfiguration%\netcoreapp1.0\simpleharness.dll
-popd
+  if not exist "%DotNet%" (
+    call :print_error_message Could not install dotnet cli correctly. See '%Init_Tools_Log%' for more details.
+    exit /b 1
+  )
+endlocal& exit /b 0
 
-goto :eof
+:build_procdomain
+setlocal
+  cd %~dp0src\procdomain
+  call :dotnet_build
+  exit /b %errorlevel%
+
+:build_xunit_performance_analysis
+setlocal
+  cd %~dp0src\xunit.performance.analysis
+  call :dotnet_pack
+  exit /b %errorlevel%
+
+:build_xunit_performance_core
+setlocal
+  cd %~dp0src\xunit.performance.core
+  call :dotnet_pack
+  exit /b %errorlevel%
+
+:build_xunit_performance_execution
+setlocal
+  cd %~dp0src\xunit.performance.execution
+  call :dotnet_pack
+  exit /b %errorlevel%
+
+:build_xunit_performance_logger
+setlocal
+  cd %~dp0src\xunit.performance.logger
+  call :dotnet_build
+  exit /b %errorlevel%
+
+:build_xunit_performance_metrics
+setlocal
+  cd %~dp0src\xunit.performance.metrics
+  call :dotnet_pack
+  exit /b %errorlevel%
+
+:build_xunit_performance_run
+setlocal
+  cd %~dp0src\xunit.performance.run
+  call :dotnet_build
+  exit /b %errorlevel%
+
+:build_microsoft_dotnet_xunit_performance_runner_cli
+setlocal
+  cd %~dp0src\cli\Microsoft.DotNet.xunit.performance.runner.cli
+  call :dotnet_pack
+  exit /b %errorlevel%
+
+:build_microsoft_dotnet_xunit_performance_analysis_cli
+setlocal
+  cd %~dp0src\cli\Microsoft.DotNet.xunit.performance.analysis.cli
+  call :dotnet_pack
+  exit /b %errorlevel%
+
+:build_samples_classlibrary_net46
+setlocal
+  cd %~dp0samples\ClassLibrary.net46
+  call :dotnet_build
+  exit /b %errorlevel%
+
+:build_samples_simpleperftests
+setlocal
+  cd %~dp0samples\SimplePerfTests
+  call :dotnet_build
+  exit /b %errorlevel%
+
+:nuget_pack_src
+setlocal
+  cd %~dp0src
+  call %DotNet% nuget pack xunit.performance.nuspec                -p Configuration=%BuildConfiguration% --version=%PackageVersion% --output-directory %OutputDirectory% --symbols  || exit /b 1
+  call %DotNet% nuget pack xunit.performance.runner.Windows.nuspec -p Configuration=%BuildConfiguration% --version=%PackageVersion% --output-directory %OutputDirectory% --symbols  || exit /b 1
+  exit /b 0
+
+:build_xunit_performance_api
+setlocal
+  cd %~dp0src\xunit.performance.api
+  call :dotnet_pack
+  exit /b %errorlevel%
+
+:build_tests_simpleharness
+setlocal
+  cd %~dp0tests\simpleharness
+  call :dotnet_build  || exit /b 1
+  net.exe session 1>nul 2>&1 || (
+    call :print_error_message Cannot run simpleharness test because this is not an administrator window
+    exit /b 1
+  )
+  call %DotNet% run -c %BuildConfiguration% bin\%BuildConfiguration%\netcoreapp1.0\simpleharness.dll                                                                                                        || exit /b 1
+  exit /b %errorlevel%
+
+:dotnet_build
+  echo/
+  echo/  ==========
+  echo/   Building %cd%
+  echo/  ==========
+  call :remove_directory bin                                                    || exit /b 1
+  call :remove_directory obj                                                    || exit /b 1
+  call %DotNet% restore                                                         || exit /b 1
+  call %DotNet% build -c %BuildConfiguration% --version-suffix %VersionSuffix%  || exit /b 1
+  exit /b 0
+
+:dotnet_pack
+  echo/
+  echo/  ==========
+  echo/   Packing %cd%
+  echo/  ==========
+  call :remove_directory bin                                                                                                || exit /b 1
+  call :remove_directory obj                                                                                                || exit /b 1
+  call %DotNet% restore                                                                                                     || exit /b 1
+  call %DotNet% build -c %BuildConfiguration% --version-suffix %VersionSuffix%                                              || exit /b 1
+  call %DotNet% pack  -c %BuildConfiguration% --version-suffix %VersionSuffix% --output %OutputDirectory% --include-symbols || exit /b 1
+
+:: FIXME: pack sources does not work with the current mixed version of the Tracing library (EXCEPTION THROWN).
+::call %DotNet% pack  -c %BuildConfiguration% --version-suffix %VersionSuffix% --output %OutputDirectory% --include-symbols --include-source  || exit /b 1
+  exit /b 0
+
+:print_error_message
+  echo/
+  echo/  [ERROR] %*
+  echo/
+  exit /b %errorlevel%
+
+:remove_directory
+  if "%~1" == "" (
+    call :print_error_message Directory name was not specified.
+    exit /b 1
+  )
+  if exist "%~1" rmdir /s /q "%~1"
+  if exist "%~1" (
+    call :print_error_message Failed to remove directory "%~1".
+    exit /b 1
+  )
+  exit /b 0
--- src/xunit.performance.api/XunitPerformanceHarness.cs
+++ src/xunit.performance.api/XunitPerformanceHarness.cs
@@ -1,3 +1,4 @@
+using MarkdownLog;
 using Microsoft.Xunit.Performance.Api.Table;
 using System;
 using System.Collections.Generic;
@@ -46,7 +47,7 @@ namespace Microsoft.Xunit.Performance.Api
         private void ProcessResults()
         {
             var reader = new CSVMetricReader(Configuration.FileLogPath);
-            WriteStatisticsToFile(reader);
+            WriteStatisticsToFile(Configuration.RunId, reader);
 
 #if !DEBUG
             // Deleting raw data not used by api users.
@@ -54,22 +55,8 @@ namespace Microsoft.Xunit.Performance.Api
 #endif
         }
 
-        /// <summary>
-        /// Generate CSV data (Probably not the most efficient way, e.g. How big can this become).
-        /// </summary>
-        /// <param name="reader"></param>
-        private void WriteStatisticsToFile(CSVMetricReader reader)
+        private static IEnumerable<(string testCaseName, string metric, IEnumerable<double> values)> GetMeasurements(CSVMetricReader reader)
         {
-            // FIXME: Update the statistics file name.
-            var statisticsTable = new DataTable();
-            var col0_testName = statisticsTable.AddColumn("Test Name");
-            var col1_metric = statisticsTable.AddColumn("Metric");
-            var col2_iterations = statisticsTable.AddColumn("Iterations");
-            var col3_average = statisticsTable.AddColumn("Average");
-            var col4_stdevs = statisticsTable.AddColumn("Sample standard deviation");
-            var col5_min = statisticsTable.AddColumn("Minimum");
-            var col6_max = statisticsTable.AddColumn("Maximum");
-
             foreach (var testCaseName in reader.TestCases)
             {
                 var iterations = reader.GetValues(testCaseName);
@@ -86,31 +73,110 @@ namespace Microsoft.Xunit.Performance.Api
                 }
 
                 foreach (var measurement in measurements)
-                {
-                    var metric = measurement.Key;
-                    var count = measurement.Value.Count;
-                    var avg = measurement.Value.Average();
-                    var stdev_s = Math.Sqrt(measurement.Value.Sum(x => Math.Pow(x - avg, 2)) / (measurement.Value.Count - 1));
-                    var max = measurement.Value.Max();
-                    var min = measurement.Value.Min();
-
-                    var r = statisticsTable.AppendRow();
-                    r[col0_testName] = testCaseName;
-                    r[col1_metric] = metric;
-                    r[col2_iterations] = count.ToString();
-                    r[col3_average] = avg.ToString();
-                    r[col4_stdevs] = stdev_s.ToString();
-                    r[col5_min] = min.ToString();
-                    r[col6_max] = max.ToString();
-                }
+                    yield return (testCaseName, measurement.Key, measurement.Value);
             }
-
-            var statisticsFilePath = $"{Configuration.RunId}-Statistics.csv";
-            statisticsTable.WriteToCSV(statisticsFilePath);
-            WriteInfoLine($"Statistics written to \"{statisticsFilePath}\"");
         }
 
-#region IDisposable implementation
+        private static DataTable CreateStatistics(CSVMetricReader reader)
+        {
+            var statisticsTable = new DataTable();
+            var col0_testName = statisticsTable.AddColumn("Test Name");
+            var col1_metric = statisticsTable.AddColumn("Metric");
+            var col2_iterations = statisticsTable.AddColumn("Iterations");
+            var col3_average = statisticsTable.AddColumn("Average");
+            var col4_stdevs = statisticsTable.AddColumn("Sample standard deviation");
+            var col5_min = statisticsTable.AddColumn("Minimum");
+            var col6_max = statisticsTable.AddColumn("Maximum");
+
+            foreach (var (testCaseName, metric, values) in GetMeasurements(reader))
+            {
+                var count = values.Count();
+                var avg = values.Average();
+                var stdev_s = Math.Sqrt(values.Sum(x => Math.Pow(x - avg, 2)) / (values.Count() - 1));
+                var max = values.Max();
+                var min = values.Min();
+
+                var newRow = statisticsTable.AppendRow();
+                newRow[col0_testName] = testCaseName;
+                newRow[col1_metric] = metric;
+                newRow[col2_iterations] = count.ToString();
+                newRow[col3_average] = avg.ToString();
+                newRow[col4_stdevs] = stdev_s.ToString();
+                newRow[col5_min] = min.ToString();
+                newRow[col6_max] = max.ToString();
+            }
+
+            return statisticsTable;
+        }
+
+        private static void PrintStatistics(DataTable statisticsTable)
+        {
+            var cellValueFunctions = new Func<Row, object>[] {
+                (row) => {
+                    return row[statisticsTable.ColumnNames["Test Name"]];
+                },
+                (row) => {
+                    return row[statisticsTable.ColumnNames["Metric"]];
+                },
+                (row) => {
+                    return row[statisticsTable.ColumnNames["Iterations"]];
+                },
+                (row) => {
+                    return row[statisticsTable.ColumnNames["Average"]];
+                },
+                (row) => {
+                    return row[statisticsTable.ColumnNames["Sample standard deviation"]];
+                },
+                (row) => {
+                    return row[statisticsTable.ColumnNames["Minimum"]];
+                },
+                (row) => {
+                    return row[statisticsTable.ColumnNames["Maximum"]];
+                },
+            };
+
+            var mdTable = statisticsTable.Rows.ToMarkdownTable(cellValueFunctions);
+            mdTable.Columns = from column in statisticsTable.ColumnNames
+                              select new TableColumn
+                              {
+                                  HeaderCell = new TableCell() { Text = column.Name }
+                              };
+            mdTable.Columns = new TableColumn[]
+            {
+                new TableColumn(){ HeaderCell = new TableCell() { Text = "Test Name" }, Alignment = TableColumnAlignment.Left },
+                new TableColumn(){ HeaderCell = new TableCell() { Text = "Metric" }, Alignment = TableColumnAlignment.Left },
+                new TableColumn(){ HeaderCell = new TableCell() { Text = "Iterations" }, Alignment = TableColumnAlignment.Center },
+                new TableColumn(){ HeaderCell = new TableCell() { Text = "Average" }, Alignment = TableColumnAlignment.Right },
+                new TableColumn(){ HeaderCell = new TableCell() { Text = "Sample standard deviation" }, Alignment = TableColumnAlignment.Right },
+                new TableColumn(){ HeaderCell = new TableCell() { Text = "Minimum" }, Alignment = TableColumnAlignment.Right },
+                new TableColumn(){ HeaderCell = new TableCell() { Text = "Maximum" }, Alignment = TableColumnAlignment.Right },
+            };
+
+            // TODO: Print table as Markdown!
+            Console.WriteLine(mdTable);
+        }
+
+        /// <summary>
+        /// Generate CSV data (Probably not the most efficient way, e.g. How big can this become).
+        /// </summary>
+        /// <param name="reader"></param>
+        private static void WriteStatisticsToFile(string configurationRunId, CSVMetricReader reader)
+        {
+            var statisticsTable = CreateStatistics(reader);
+
+            // Only create output statistics if there is data.
+            if (statisticsTable.Rows.Count() > 0)
+            {
+                PrintStatistics(statisticsTable);
+
+                var currentWorkingDirectory = Directory.GetCurrentDirectory();
+                var statisticsFilePath = Path.Combine(currentWorkingDirectory, $"{configurationRunId}-Statistics.csv");
+                statisticsTable.WriteToCSV(statisticsFilePath);
+                WriteInfoLine($"Statistics written to \"{statisticsFilePath}\"");
+            }
+        }
+
+        #region IDisposable implementation
 
         public void Dispose()
         {
@@ -148,7 +214,7 @@ namespace Microsoft.Xunit.Performance.Api
         {
         }
 
-#endregion IDisposable implementation
+        #endregion IDisposable implementation
 
         private readonly string[] _args;
         private bool _disposed;
