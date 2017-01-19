06451763c28f13913f46843196cb4598c4d915ed
Jose Rivero <jorive@microsoft.com>
2017-01-19 12:17:49 -0800

    Updated BuildForPublication script to do a fresh download/build.

--- BuildForPublication.cmd
+++ BuildForPublication.cmd
@@ -5,12 +5,19 @@ goto :main
 setlocal
   set errorlevel=
   set BuildConfiguration=Release
-  set VersionSuffix=build0044
+  set VersionSuffix=build0047
   set PackageVersion=1.0.0-alpha-%VersionSuffix%
 
   echo/==================
   echo/ Building version %PackageVersion% NuGet packages.
   echo/==================
+
+  set LocalDotNet_ToolsDir=%~dp0tools
+  if exist "%LocalDotNet_ToolsDir%" rmdir /s /q "%LocalDotNet_ToolsDir%"
+  if exist "%LocalDotNet_ToolsDir%" (
+    echo ERROR: Failed to remove "%LocalDotNet_ToolsDir%" folder.
+    exit /b 1
+  )
 
   call build.cmd %BuildConfiguration% %VersionSuffix% %PackageVersion%
 endlocal& exit /b %errorlevel%
