7965d9e291a2e2ea412e7541b7b5214cf63a7a00
José Rivero <jorive@microsoft.com>
2017-02-22 13:31:08 -0800

    Updating command line options and csproj to build with latest VS2017

--- src/xunit.performance.api/XunitPerformanceHarnessOptions.cs
+++ src/xunit.performance.api/XunitPerformanceHarnessOptions.cs
@@ -4,14 +4,12 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using static Microsoft.Xunit.Performance.Api.XunitPerformanceLogger;
 
 namespace Microsoft.Xunit.Performance.Api
 {
     /// <summary>
     /// Xunit Performance Harness Api command line options.
     /// </summary>
-    [Verb("perf-api", HelpText = "Performance API options.")]
     internal sealed class XunitPerformanceHarnessOptions
     {
         public XunitPerformanceHarnessOptions()
@@ -21,7 +19,7 @@ namespace Microsoft.Xunit.Performance.Api
             _typeNames = new List<string>();
         }
 
-        [Option('o', "outputdir", Required = false, HelpText = "Specifies the output directory name.")]
+        [Option("perf:outputdir", Required = false, HelpText = "Specifies the output directory name.")]
         public string OutputDirectory
         {
             get { return _outputDirectory; }
@@ -46,7 +44,7 @@ namespace Microsoft.Xunit.Performance.Api
             }
         }
 
-        [Option("id", Required = false, HelpText = "User defined id given to the performance harness.")]
+        [Option("perf:id", Required = false, HelpText = "User defined id given to the performance harness.")]
         public string RunId
         {
             get { return _runid; }
@@ -67,7 +65,7 @@ namespace Microsoft.Xunit.Performance.Api
             }
         }
 
-        [Option('t', "typenames", Required = false, Separator = ',',
+        [Option("perf:typenames", Required = false, Separator = ',',
             HelpText = "The (optional) type names of the test classes to run.")]
         public IEnumerable<string> TypeNames
         {
@@ -82,77 +80,69 @@ namespace Microsoft.Xunit.Performance.Api
 
         public static XunitPerformanceHarnessOptions Parse(string[] args)
         {
-            XunitPerformanceHarnessOptions options = null;
+            using (var parser = new Parser((settings) =>
+            {
+                settings.CaseInsensitiveEnumValues = true;
+                settings.CaseSensitive = false;
+                settings.HelpWriter = new StringWriter();
+                settings.IgnoreUnknownArguments = true;
+            }))
+            {
+                XunitPerformanceHarnessOptions options = null;
+                var parserResult = parser.ParseArguments<XunitPerformanceHarnessOptions>(args)
+                    .WithParsed(parsed => options = parsed)
+                    .WithNotParsed(errors =>
+                    {
+                        foreach (var error in errors)
+                        {
+                            switch (error.Tag)
+                            {
+                                case ErrorType.MissingValueOptionError:
+                                    throw new ArgumentException(
+                                        $"Missing value option for {(error as MissingValueOptionError).NameInfo.NameText}");
+                                case ErrorType.HelpRequestedError:
+                                    Console.WriteLine(Usage());
+                                    Environment.Exit(0);
+                                    break;
+                                case ErrorType.BadFormatTokenError:
+                                case ErrorType.UnknownOptionError:
+                                case ErrorType.MissingRequiredOptionError:
+                                case ErrorType.MutuallyExclusiveSetError:
+                                case ErrorType.BadFormatConversionError:
+                                case ErrorType.SequenceOutOfRangeError:
+                                case ErrorType.RepeatedOptionError:
+                                case ErrorType.NoVerbSelectedError:
+                                case ErrorType.BadVerbSelectedError:
+                                case ErrorType.HelpVerbRequestedError:
+                                    break;
+                            }
+                        }
+                    });
+                return options;
+            }
+        }
+
+        public static string Usage()
+        {
             var parser = new Parser((parserSettings) =>
             {
                 parserSettings.CaseInsensitiveEnumValues = true;
                 parserSettings.CaseSensitive = false;
                 parserSettings.EnableDashDash = true;
-                //parserSettings.IgnoreUnknownArguments = true;
+                parserSettings.HelpWriter = new StringWriter();
+                parserSettings.IgnoreUnknownArguments = true;
             });
-            var parserResult = parser.ParseArguments<XunitPerformanceHarnessOptions>(args)
-                .WithParsed(parsed => options = parsed)
-                .WithNotParsed(errors =>
-                {
-                    foreach (var error in errors)
-                    {
-                        switch (error.Tag)
-                        {
-                            case ErrorType.MissingValueOptionError:
-                                WriteErrorLine($"Missing value option for {(error as MissingValueOptionError).NameInfo.NameText}");
-                                Environment.Exit(1);
-                                break;
-                            case ErrorType.HelpRequestedError:
-                                Console.WriteLine(Usage());
-                                Environment.Exit(0);
-                                break;
-                            case ErrorType.VersionRequestedError:
-                                Console.WriteLine("TODO"); // TODO: Get this assembly version.
-                                //(typeof(Microsoft.Xunit.Performance.Api.XunitPerformanceHarness) as System.RuntimeType).Assembly.Location
-                                Environment.Exit(0);
-                                break;
-                            case ErrorType.BadFormatTokenError:
-                            case ErrorType.UnknownOptionError:
-                            case ErrorType.MissingRequiredOptionError:
-                            case ErrorType.MutuallyExclusiveSetError:
-                            case ErrorType.BadFormatConversionError:
-                            case ErrorType.SequenceOutOfRangeError:
-                            case ErrorType.RepeatedOptionError:
-                            case ErrorType.NoVerbSelectedError:
-                            case ErrorType.BadVerbSelectedError:
-                            case ErrorType.HelpVerbRequestedError:
-                                break;
-                        }
-                    }
-                });
-            return options;
-        }
+            var result = parser.ParseArguments<XunitPerformanceHarnessOptions>(new string[] { "--help" });
 
-        public static string Usage()
-        {
-            var helpText = new HelpText
+            var helpTextString = new HelpText
             {
                 AddDashesToOption = true,
                 AddEnumValuesToHelpText = true,
                 AdditionalNewLineAfterOption = false,
                 Copyright = "Copyright (c) Microsoft Corporation 2015",
                 Heading = "Xunit-Performance-Api",
-                MaximumDisplayWidth = 80
-            };
-
-            var helpWriter = new StringWriter();
-            var parser = new Parser((parserSettings) =>
-            {
-                parserSettings.CaseInsensitiveEnumValues = true;
-                parserSettings.CaseSensitive = false;
-                parserSettings.EnableDashDash = true;
-                parserSettings.HelpWriter = helpWriter;
-                parserSettings.IgnoreUnknownArguments = true;
-            });
-            var result = parser.ParseArguments(new string[] { "perf-api", "--help" }, typeof(XunitPerformanceHarnessOptions));
-            helpText.AddOptions(result);
-
-            var helpTextString = helpText.ToString();
+                MaximumDisplayWidth = 80,
+            }.AddOptions(result).ToString();
             return helpTextString;
         }
 
