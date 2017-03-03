46a20e8ea09bfd0bc9920c7619a320e9cbfd1687
José Rivero <jorive@microsoft.com>
2017-03-03 12:46:52 -0800

    Pushing the version up ahead with enough room from the current master branch.

--- BuildForPublication.cmd
+++ BuildForPublication.cmd
@@ -4,8 +4,8 @@ goto :main
 :main
 setlocal
 
-set PackageVersion=1.0.0-alpha-build0041
-set VersionSuffix=build0041
+set PackageVersion=1.0.0-alpha-build0100
+set VersionSuffix=build0100
 
 echo Building version %PackageVersion% NuGet packages.
 
