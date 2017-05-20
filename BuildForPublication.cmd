a7d6a9ead4c65816a0ebc1958b2ca54347f780cf
nulltoken <emeric.fermas@gmail.com>
2017-05-20 11:12:22 +0200

    Bump NuGet package version

--- BuildForPublication.cmd
+++ BuildForPublication.cmd
@@ -5,7 +5,7 @@
 setlocal EnableDelayedExpansion
   set errorlevel=
   set BuildConfiguration=Release
-  set VersionSuffix=beta-build0004
+  set VersionSuffix=beta-build0005
 
   REM Check that git is on path.
   where.exe /Q git.exe || (
