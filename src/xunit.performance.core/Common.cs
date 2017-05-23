0de1129399d64963af0ae331956dfbfe9986c633
José Rivero <jorive@microsoft.com>
2017-05-22 20:00:51 -0700

    Adding missing file.

---
+++ src/xunit.performance.core/Common.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Microsoft.Xunit.Performance.Api
+{
+    /// <summary>
+    /// This class provides common utilities to the API.
+    /// </summary>
+    internal static class Common
+    {
+        public static bool IsWindowsPlatform => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+    }
+}
