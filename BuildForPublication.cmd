0c4a6f381bb99c95bfb0b1169ae8f3d86b8077b8
José Rivero <jorive@microsoft.com>
2017-11-09 21:12:46 -0800

    Bump package number

--- BuildForPublication.cmd
+++ BuildForPublication.cmd
@@ -5,7 +5,7 @@
 setlocal EnableDelayedExpansion
   set errorlevel=
   set BuildConfiguration=Release
-  set VersionSuffix=beta-build0011
+  set VersionSuffix=beta-build0012
 
   REM Check that git is on path.
   where.exe /Q git.exe || (
