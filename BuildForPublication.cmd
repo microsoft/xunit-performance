bb8d0463e38fb206748d6e20c19155124ecc0af7
José Rivero <jorive@microsoft.com>
2018-01-16 18:14:24 -0800

    Disable building net461 target on Non-Windows platforms.

--- BuildForPublication.cmd
+++ BuildForPublication.cmd
@@ -5,7 +5,7 @@
 setlocal EnableDelayedExpansion
   set errorlevel=
   set BuildConfiguration=Release
-  set VersionSuffix=beta-build0015
+  set VersionSuffix=beta-build0016
 
   REM Check that git is on path.
   where.exe /Q git.exe || (
